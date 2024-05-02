23cb9daa3ed463c8b6816c89a1b7a212ec42244d
ashish357 <ashishsaha357@gmail.com>
2024-05-02 23:01:29 +0530

    Updated Cors

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -10,7 +10,7 @@ export const YOUTUBE_VIDEOS_API =
  `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
 export const YOUTUBE_SEARCH_SUGGESTION_API = 
-"https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+"https://api.allorigins.win/raw?url="+encodeURIComponent("http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=")
 
 export const YOUTUBE_CHANNEL_DATA_API = 
 `https://www.googleapis.com/youtube/v3/channels?part=snippet%2Cstatistics&key=${GOOGLE_API_KEY}&id=`
